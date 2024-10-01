2f6312a205c1bc81ab56d7722f862c83cad0a60a
NotA0ua <demiddubrovskylite@gmail.com>
2024-10-01 23:33:05 +0300

    aa

--- hypr/scripts/PywalUpdate.sh
+++ hypr/scripts/PywalUpdate.sh
@@ -2,7 +2,6 @@ cache="$HOME/.cache/swww/HDMI-A-1"
 wallpaper=$(cat "$cache")
 cp $wallpaper ~/.config/rofi/image
 wal -i $wallpaper
-bash "$HOME/.config/hypr/scripts/themecord.sh"
 
 pkill swaync
 sleep 0.2
